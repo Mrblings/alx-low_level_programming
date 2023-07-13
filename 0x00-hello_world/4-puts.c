b9f9f447bd985e3636cdda6b2789ad08ddf74092
Victor <chinemerem8613@gmail.com>
2023-07-13 06:57:54 -0700

    puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - entry of our code
+  * Return: returns the valu of 0
+  */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle \n");
+	return (0);
+}
