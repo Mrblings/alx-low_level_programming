c3f8c30b00ba1b9f686fbd2d1f42bb9c388b77fa
Victor <chinemerem8613@gmail.com>
2023-07-14 08:41:10 -0700

    print alphalbet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * Return: returns the value of 0
+  */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
