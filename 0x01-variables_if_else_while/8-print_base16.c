e6df478533ee40d029d228af4231e4802dcfa46b
Victor <chinemerem8613@gmail.com>
2023-07-14 13:17:35 -0700

    so hard

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 /**
   * main - entry point
-  * Return: returns 0
+  * Return: return 0
   */
 int main(void)
 {
 	int i;
 	char j;
 
-	for (i = 0; i > 10; i++)
+	for (i = 0; i < 10; i++)
 		putchar(i + '0');
 	for (j = 'a'; j <= 'f'; j++)
 		putchar(j);
 	putchar('\n');
 	return (0);
 }
+
