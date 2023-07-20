4c0a1ab9d5dbbf122663ed118132883901181e6d
Victor <chinemerem8613@gmail.com>
2023-07-20 09:07:42 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_diagonal - prints slash diagonally
+* @n: displays the number of times
+* Return: returns void
+*/
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <=  n; i++)
+		{
+			for (j = 1; j <= i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
