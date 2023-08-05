2efbf06c40faed523e99fad20872d25e7f9a1dec
Victor <chinemerem8613@gmail.com>
2023-08-05 16:33:58 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* @argc: counts the number of arguments to be taken
+* @argv: Takes in the string value
+* Return: returns zero
+*/
+
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
