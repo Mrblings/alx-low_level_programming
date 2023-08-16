6e79cb378cc0e4a180bf69e19d88059a97330a98
Victor <chinemerem8613@gmail.com>
2023-08-16 15:57:55 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - contains the struct
+* @name: gives the name
+* @age: gives the age
+* @owner: gives the owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
