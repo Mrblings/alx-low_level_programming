f82b3541851c98ca1f5c3a1fa73dcf9ee7add036
victoruche <uche vicky25@gamil.com>
2023-09-11 06:03:20 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+* main - adds numbers
+* @argc: counts arguments
+* @argv: vector of arguments
+* Return: return o if sucessful
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, j, add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
